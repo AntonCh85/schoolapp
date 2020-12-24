b81f32baaf996678f4cbbbb050ef0f3b8d4746cd
anton <ac@redstart.pro>
2020-12-24 09:29:51 +0200

    social

---
+++ src/Components/postStatusFilter/postStatusFilter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './post-status-filter.css'
+
+const PostStatusFilter = () => {
+
+    return(
+        <div className={"btn-group"}>
+            <button type={"button"} className={"btn-filter"}/>
+            <button title={"klick"} type={"button"} className={"btn-filter-two align-items-center justify-content-center"}/>
+        </div>
+    )
+}
+
+export default PostStatusFilter;
---
+++ src/Components/searchPanel/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import SearchPanel from "./searchPanel";
+
+export default SearchPanel;
