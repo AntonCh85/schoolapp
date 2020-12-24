9b518a742d8bf74da58edde862f8b4f2bc65364f
anton <ac@redstart.pro>
2020-12-24 19:00:17 +0200

    filters

--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -11,13 +11,19 @@ constructor(props) {
     super(props);
     this.state = {
         data : [
-            {label: 'Going to learn React', important: true, id:1},
-            {label: 'Going to  React', important: false, id:2},
-            {label: 'React is the best!', important: false, id: 3},
-        ]
+            {label: 'Going to learn React', important: true,like: false, id:1},
+            {label: 'Going to  React', important: false,like: false, id:2},
+            {label: 'React is the best!', important: false,like: false, id: 3},
+        ],
+        term: '',
+        filter: 'all'
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+    this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
 
     this.maxId = 4;
 }
@@ -44,18 +50,78 @@ addItem(body) {
         }
     })
 }
+onToggleImportant(id) {
+    this.setState(({data}) => {
+        const index = data.findIndex(elem => elem.id === id);
+        const old = data[index];
+        const newItem = {...old, important: !old.important};
+        const newArr = [...data.slice(0, index), newItem,...data.slice(index + 1)];
+        return {
+            data: newArr
+        }
+    })
+}
+onToggleLiked(id) {
+    this.setState(({data}) => {
+        const index = data.findIndex(elem => elem.id === id);
+        const old = data[index];
+        const newItem = {...old, like: !old.like};
+        const newArr = [...data.slice(0, index), newItem,...data.slice(index + 1)];
+        return {
+            data: newArr
+        }
+    })
+}
 
+searchPosts(items, term) {
+    if (term.length === 0) {
+        return items
+    }
+   return  items.filter((item) => {
+        return item.label.indexOf(term) > -1
+    });
+}
+
+filterPosts(items, filter) {
+    if (filter === 'like') {
+        return items.filter(item => item.like);
+    } else {
+        return items
+    }
+}
+onFilterSelect(filter) {
+    this.setState({filter});
+}
+
+onUpdateSearch(term) {
+    this.setState({term});
+}
     render() {
-
+    const {data, term, filter} = this.state;
+const liked = data.filter(item => item.like).length;
+const allPosts = data.length;
+const visiblePosts = this.filterPosts(this.searchPosts(data, term), filter);
         return(
             <div className={"app"}>
-                <AppHeader/>
+                <AppHeader
+                    liked={liked}
+                    allPosts={allPosts}
+                />
                 <div className={"search-panel d-flex"}>
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}
+
+
+                    />
+                    <PostStatusFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}
+                    />
                 </div>
-                <PostList posts={this.state.data}
+                <PostList posts={visiblePosts}
                           onDelete={this.deleteItem}
+                          onToggleImportant={this.onToggleImportant}
+                          onToggleLiked={this.onToggleLiked}
 
                 />
                 <PostAddForm
--- src/Components/appHeader/appHeader.js
+++ src/Components/appHeader/appHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './app-header.css'
 
-const AppHeader = () => {
+const AppHeader = ({liked,allPosts}) => {
 
     return(
         <div className={"app-header d-flex"}>
             <h1 className={"h1-color"}>Chepizhenko Anton</h1>
-            <h2>5 записей, из них понравилось 0 </h2>
+            <h2>{allPosts} записей, из них понравилось {liked} </h2>
         </div>
     )
 }
